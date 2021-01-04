a768d4c58c8106eadbcb378376dd35cfcc56a829
sberk97 <sebastian.berk97@gmail.com>
2021-01-04 22:41:02 +0100

    Added CountryListAdapter and CountryListItem

--- app/src/main/java/com/example/covid19countryinfo/SearchCountry.java
+++ app/src/main/java/com/example/covid19countryinfo/SearchCountry.java
@@ -1,8 +1,11 @@
 package com.example.covid19countryinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SearchCountry extends AppCompatActivity {
 
