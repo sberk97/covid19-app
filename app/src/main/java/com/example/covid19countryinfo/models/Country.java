170710cf7639df1315f715abe36aaf4cf92fc9d6
sberk97 <sebastian.berk97@gmail.com>
2021-01-26 23:25:51 +0100

    Move country update logic to model

--- app/src/main/java/com/example/covid19countryinfo/models/Country.java
+++ app/src/main/java/com/example/covid19countryinfo/models/Country.java
@@ -1,5 +1,30 @@
 package com.example.covid19countryinfo.models;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.covid19countryinfo.R;
+import com.example.covid19countryinfo.misc.Constants;
+import com.example.covid19countryinfo.misc.Helper;
+import com.example.covid19countryinfo.misc.NoCasesException;
+import com.example.covid19countryinfo.misc.RequestQueueSingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
 public class Country {
     private String countryName;
     private String countryCode;
@@ -68,5 +93,101 @@ public class Country {
 
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
+    }
+
+    public void updateData(boolean getYesterdayData, Context ctx, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener) {
+        String url = Constants.COUNTRY_DATA_API + countryCode;
+        if (getYesterdayData) {
+            url += "?yesterday=true";
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, responseListener, errorListener);
+        RequestQueueSingleton.getInstance(ctx).addToRequestQueue(jsonObjectRequest);
+    }
+
+    public boolean updateObject(boolean getYesterdayData, JSONObject response) throws JSONException, NoCasesException {
+        int latestCases = response.getInt("todayCases");
+        int latestDeaths = response.getInt("todayDeaths");
+        int latestRecovered = response.getInt("todayRecovered");
+        long epochDate = response.getLong("updated");
+
+        boolean noNewCases = newCasesInLatestData(latestCases, latestDeaths, latestRecovered);
+        boolean oldDataNoNewCases = newCasesInCurrentData();
+        boolean dataIsTheSame = isCurrentAndLatestDataTheSame(latestCases, latestDeaths, latestRecovered, epochDate);
+
+        if (dataIsTheSame) {
+            return false;
+        }
+        // If today and yesterday no new cases
+        else if (noNewCases && oldDataNoNewCases) {
+            if (getYesterdayData) {
+                epochDate = getYesterdayDate(epochDate);
+                this.setLastUpdateDate(Helper.formatDate(epochDate));
+            } else {
+                throw new NoCasesException("Latest data does not have new cases");
+            }
+        }
+        // Old data has cases, new doesn't and its not yesterday data then try getting yesterday data
+        else if (noNewCases && !getYesterdayData) {
+            throw new NoCasesException("Latest data does not have new cases");
+        }
+        // New data has cases
+        else {
+            if (getYesterdayData) {
+                epochDate = getYesterdayDate(epochDate);
+            }
+
+            this.setLatestCases(latestCases);
+            this.setLatestDeaths(latestDeaths);
+            this.setLatestRecovered(latestRecovered);
+            this.setLastUpdateDate(Helper.formatDate(epochDate));
+        }
+
+        return true;
+    }
+
+    public void updateDataDatabase(SQLiteDatabase mDb) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Constants.UPDATE_COUNTRY)
+                .append("latest_cases=")
+                .append(latestCases)
+                .append(", latest_deaths=")
+                .append(latestDeaths)
+                .append(", latest_recovered=")
+                .append(latestRecovered)
+                .append(", date='")
+                .append(dateToEpoch())
+                .append("' WHERE country_code='")
+                .append(countryCode).append("';");
+
+        mDb.execSQL(sb.toString());
+    }
+
+    private boolean newCasesInLatestData(int latestCases, int latestDeaths, int latestRecovered) {
+        return latestCases == 0 && latestDeaths == 0 && latestRecovered == 0;
+    }
+
+    private boolean newCasesInCurrentData() {
+        return this.getLatestCases() == 0 && this.getLatestDeaths() == 0 && this.getLatestRecovered() == 0;
+    }
+
+    private boolean isCurrentAndLatestDataTheSame(int latestCases, int latestDeaths, int latestRecovered, long epochDate) {
+        return this.getLatestCases() == latestCases && this.getLatestDeaths() == latestDeaths && this.getLatestRecovered() == latestRecovered && this.getLastUpdateDate().equals(Helper.formatDate(epochDate));
+    }
+
+    private long getYesterdayDate(long epochDate) {
+        return epochDate - 86400000;
+    }
+
+    private long dateToEpoch() {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.UK);
+            Date date = sdf.parse(lastUpdateDate);
+            return date.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
     }
 }
